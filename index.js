8cb12804fcfed987da4152f69dcbf43b9626f5c8
mdmishrat13 <mdmishrat13@gmail.com>
2022-04-06 21:25:16 +0600

    completed everything

--- index.js
+++ index.js
@@ -3,49 +3,69 @@ const cors = require('cors');
 const { json } = require('express/lib/response');
 const port = process.env.PORT || 5000;
 const app = express();
+const {MongoClient}= require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 
-const users=[
-    {
-    "id":0,
-    "name":"md mishrat",
-    "email":"mdmishrat13@gmail.com"
-    },
-    {
-    "id":1,
-    "name":"md mishrat",
-    "email":"mdmishrat13@gmail.com"
-    },
-    {
-    "id":2,
-    "name":"md mishrat",
-    "email":"mdmishrat13@gmail.com"
-    },
-    {
-    "id":3,
-    "name":"md mishrat",
-    "email":"mdmishrat13@gmail.com"
-    },
-    {
-    "id":3,
-    "name":"md mishrat",
-    "email":"mdmishrat13@gmail.com"
-    }
-]
 
 app.use(cors());
 app.use(express.json())
 
-app.get('/users',(req,res)=>{
-    res.send(users)
-})
+const uri = `mongodb+srv://Mdmishrat13:MdLikhon@cluster0.fkofk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.post('/users',(req,res)=>{
-    const newUser= req.body;
-    newUser.id = users.length;
-    users.push(newUser);
-    res.json(newUser)
-})
 
+async function run(){
+    try{
+        await client.connect()
+        const database = client.db('crudApp');
+        const userCollection= database.collection("users")
+
+        app.post('/users',async(req,res)=>{
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser)
+            res.json(result)
+        });
+
+        app.get('/users',async(req,res)=>{
+            const cursor = userCollection.find({})
+            const users = await cursor.toArray()
+            res.json(users)
+        });
+        app.delete('/users/:id',async(req,res)=>{
+            const id= req.params.id;
+            const quary = { _id:ObjectId(id)}
+            const result = await userCollection.deleteOne(quary);
+            res.json(result)
+        })
+        app.get('/users/:id',async(req,res)=>{
+            const id = req.params.id;
+            const quary = {_id:ObjectId(id)}
+            const user = await userCollection.findOne(quary)
+            res.json(user)
+
+        })
+        app.put('/users/:id',async(req,res)=>{
+           const id = req.params.id;
+           const updatedUser= req.body;
+           const search = {_id:ObjectId(id)};
+           const options = {upsert:false}
+           const updateDoc = {
+               $set:{
+                   name:updatedUser.name,
+                   email:updatedUser.email
+               }
+           };
+           const result = await userCollection.updateOne(search,updateDoc,options)
+           res.json(result)
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+    
 app.listen(port,()=>{
     console.log('listening port ',port)
 })
